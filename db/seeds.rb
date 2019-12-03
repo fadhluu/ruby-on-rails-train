1fb310a7b029fd0e26c07d459f8c2b31ebd5564a
fadhlu <fadhlurahman.i@gmail.com>
2019-12-03 16:24:34 +0700

    add tables articles

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Article.create([
+    {
+        title: 'Pencurian',
+        content: 'Lorem ipsum blabla'
+    },
+    {
+        title: 'Pencurian terjadi lagi',
+        content: 'Lorem ipsum bla2x'
+    }
+])
