332903d6d917763eb221a9221bd3868971345116
fadhlu <fadhlurahman.i@gmail.com>
2019-12-12 11:05:18 +0700

    init new env

--- db/schema.rb
+++ db/schema.rb
@@ -42,7 +42,6 @@ ActiveRecord::Schema.define(version: 2019_12_10_074819) do
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "password_hash"
     t.string "password_salt"
   end
 
